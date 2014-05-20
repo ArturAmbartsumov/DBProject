7cd8de679010aa1861447e43994688c052430c4d
ArturAmbartsumov <Artur_pskov@mail.ru>
2014-05-20 15:39:29 +0400

    Добавлен мидл

--- DBProject/settings.py
+++ DBProject/settings.py
@@ -93,6 +93,7 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
